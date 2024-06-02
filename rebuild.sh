abaee35e6622f83a81a33a27f92dd9ddb998b50c
ayes <github.com.faculty419@passmail.net>
2024-06-02 11:32:51 +0200

    109 current  2024-06-02 11:15:27  24.11.20240529.ad57eef       6.9.2-zen1                          *

--- rebuild.sh
+++ rebuild.sh
@@ -9,6 +9,22 @@ if git diff --quiet '*.nix' ./config; then
     exit 0
 fi
 
+while getopts "t" OPTION; do
+	case $OPTION in
+		t)
+			isTestmode=true;
+			;;
+		*)
+			echo "Usage:"
+			echo "rebuild -t"
+			echo ""
+			echo "	-t		execute rebuild in test mode"
+			exit 0
+			;;
+	esac
+done
+
+
 # Autoformat your nix files
 #alejandra . &>/dev/null \
 #  || ( alejandra . ; echo "formatting failed!" && exit 1)
@@ -19,7 +35,11 @@ git diff -U0 '*.nix' ./config/
 echo "NixOs Rebuilding..."
 
 # Rebuild, output simplified errors, log tracebacks
-sudo nixos-rebuild switch --flake ~/.nixos/ &>nixos-switch.log || (cat nixos-switch.log | grep --color error && exit 1)
+if [ "$isTestmode" = true ]; then
+    sudo nixos-rebuild test --flake ~/.nixos/
+else
+	sudo nixos-rebuild switch --flake ~/.nixos/ &>nixos-switch.log || (cat nixos-switch.log | grep --color error && exit 1)
+fi
 
 # Get current generation metadata
 current=$(nixos-rebuild list-generations | grep current)
