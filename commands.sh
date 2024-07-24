fa25e6ea6a140aa09c0bf626fd4f826078cce3df
ayes <github.com.faculty419@passmail.net>
2024-07-25 00:34:29 +0200

    Added an upgrade script
    This script prints out the version updates that are about to happen before actually applying them

--- commands.sh
+++ commands.sh
@@ -1,7 +1,7 @@
 #! /run/current-system/sw/bin/bash
 export nixosConfigPath="$HOME/.nixos"
 export update="nix flake update $nixosConfigPath"
-export rebuild="nixos-rebuild $1 --use-remote-sudo --flake $nixosConfigPath --option eval-cache false"
+export rebuild="nixos-rebuild --use-remote-sudo --flake $nixosConfigPath --option eval-cache false"
 
 export return="popd"
 
--- upgrade.sh
+++ upgrade.sh
@@ -1,7 +1,21 @@
 #! /run/current-system/sw/bin/bash
-. ./commands.sh switch
+. ./commands.sh
 
 $update
-$rebuild --upgrade
+$rebuild build
+
+echo ''
+nix store diff-closures /run/current-system ./result | awk '/[0-9] →|→ [0-9]/ && !/nixos/' || echo
+
+read -p $'\nProceed with update? (Y|n) ' answer
+
+case ${answer:0:1} in
+	n|N )
+
+	;;
+	* )
+		$rebuild switch
+	;;
+esac
 
 $return
