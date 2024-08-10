54979c53d1733821120a14de2d1b81294a2de705
ayes <github.com.faculty419@passmail.net>
2024-08-10 14:44:50 +0200

    239 current  2024-08-10 14:44:45  24.11.20240804.cb9a96f  6.10.1-zen1                          *

--- configuration.nix
+++ configuration.nix
@@ -9,41 +9,6 @@
 			powerOnBoot = true;
 		};
 
-		/*nvidia = {
-			open = false;
-			nvidiaSettings = true;
-			modesetting.enable = true;
-			
-			powerManagement = {
-				enable = false;
-				finegrained = false;
-			};
-
-			package = config.boot.kernelPackages.nvidiaPackages.beta;
-			*//*package = let
-					rcu_patch = pkgs.fetchpatch {
-						url = "https://github.com/gentoo/gentoo/raw/c64caf53/x11-drivers/nvidia-drivers/files/nvidia-drivers-470.223.02-gpl-pfn_valid.patch";
-						hash = "sha256-eZiQQp2S/asE7MfGvfe6dA/kdCvek9SYa/FFGp24dVg=";
-					};
-			in config.boot.kernelPackages.nvidiaPackages.mkDriver {
-				version = "535.154.05";
-				sha256_64bit = "sha256-fpUGXKprgt6SYRDxSCemGXLrEsIA6GOinp+0eGbqqJg=";
-				sha256_aarch64 = "sha256-G0/GiObf/BZMkzzET8HQjdIcvCSqB1uhsinro2HLK9k=";
-				openSha256 = "sha256-wvRdHguGLxS0mR06P5Qi++pDJBCF8pJ8hr4T8O6TJIo=";
-				settingsSha256 = "sha256-9wqoDEWY4I7weWW05F4igj1Gj9wjHsREFMztfEmqm10=";
-				persistencedSha256 = "sha256-d0Q3Lk80JqkS1B54Mahu2yY/WocOqFFbZVBh+ToGhaE=";
-
-				#version = "550.40.07";
-				#sha256_64bit = "sha256-KYk2xye37v7ZW7h+uNJM/u8fNf7KyGTZjiaU03dJpK0=";
-				#sha256_aarch64 = "sha256-AV7KgRXYaQGBFl7zuRcfnTGr8rS5n13nGUIe3mJTXb4=";
-				#openSha256 = "sha256-mRUTEWVsbjq+psVe+kAT6MjyZuLkG2yRDxCMvDJRL1I=";
-				#settingsSha256 = "sha256-c30AQa4g4a1EHmaEu1yc05oqY01y+IusbBuq+P6rMCs=";
-				#persistencedSha256 = "sha256-11tLSY8uUIl4X/roNnxf5yS2PQvHvoNjnd2CB67e870=";
-
-				patches = [ rcu_patch ];
-			};*//*
-		};*/
-
 		logitech.wireless = {
 			enable = true;
 		};
@@ -66,8 +31,9 @@
 	boot = {
 		kernelPackages = pkgs.linuxPackages_zen;
 		#kernelPackages = pkgs.linuxPackages;
-		kernelParams = [
-			#"nvidia_drm.fbdev=1"
+
+		kernelModules = [
+			"amdgpu"
 		];
 
 		loader = {
@@ -228,11 +194,9 @@
 			QT_QPA_PLATFORMTHEME = "qt6ct";
 
 			XDG_SESSION_TYPE = "wayland";
-			#LIBVA_DRIVER_NAME = "nvidia";
-			#__GLX_VENDOR_LIBRARY_NAME = "nvidia";
-			#GBM_BACKEND = "nvidia-drm";
-			NVD_BACKEND = "direct";
 			WLR_NO_HARDWARE_CURSORS = "1";
+
+			AMD_VULKAN_ICD = "RADV";
 		};
 
 		systemPackages = with pkgs; [
@@ -293,7 +257,7 @@
 			xkb.layout = "de";
 			xkb.variant = "nodeadkeys";
 
-			#videoDrivers = [ "nvidia" ];
+			videoDrivers = [ "amdgpu" ];
 		};
 
 		gnome.gnome-keyring.enable = true;
