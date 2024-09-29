d09993a68c2ffb45c64af7cf019fd0f6d520b7df
ayes <github.com.faculty419@passmail.net>
2024-09-29 02:34:11 +0200

    295 current  2024-09-29 02:34:09  24.11.20240921.9357f4f  6.10.9-zen1                          *

--- home.nix
+++ home.nix
@@ -1,399 +1,399 @@
 { config, pkgs, inputs, lib, nixpkgs-alternate, ... }: {
-	imports = [
-		./config/vesktop.nix
-		./config/hypr/hyprland.base.nix
-		inputs.ags.homeManagerModules.default
-	];
+  imports = [
+    ./config/vesktop.nix
+    ./config/hypr/hyprland.base.nix
+    inputs.ags.homeManagerModules.default
+  ];
 
-	home = {
-		username = "ayes";
-		homeDirectory = lib.mkForce "/home/ayes";
-		stateVersion = "23.11";
+  home = {
+    username = "ayes";
+    homeDirectory = lib.mkForce "/home/ayes";
+    stateVersion = "23.11";
 
-		packages = with pkgs; [
-			## Systm
-			btop
-			micro
-			pcmanfm
-			obs-studio
-			xarchiver
+    packages = with pkgs; [
+      ## Systm
+      btop
+      micro
+      pcmanfm
+      obs-studio
+      xarchiver
 
-			##hyprstuff
-			inputs.hyprsome.packages.${pkgs.stdenv.hostPlatform.system}.default
-			hyprshot
+      ##hyprstuff
+      inputs.hyprsome.packages.${pkgs.stdenv.hostPlatform.system}.default
+      hyprshot
 
-			## Media
-			bitwig-studio
-			haruna
-			obsidian
-			gpu-screen-recorder-gtk
+      ## Media
+      bitwig-studio
+      haruna
+      obsidian
+      gpu-screen-recorder-gtk
 
-			## configuration Utils
-			solaar
-			font-manager
+      ## configuration Utils
+      solaar
+      font-manager
 
-			## Community
-			vesktop
+      ## Community
+      vesktop
 
-			## Gaming
-			xivlauncher
-			prismlauncher
-			heroic
-			lutris
+      ## Gaming
+      xivlauncher
+      prismlauncher
+      heroic
+      lutris
 
-			## Dev Stuff
-			jetbrains.idea-ultimate
-			nodejs
-			bun
-			sassc
-		];
+      ## Dev Stuff
+      jetbrains.idea-ultimate
+      nodejs
+      bun
+      sassc
 
-		file = {
-			"${config.xdg.configHome}/micro/colorschemes" = {
-				source = ./config/micro/colorschemes;
-				recursive = true;
-			};
+      graalvm-ce
+    ];
 
-			"${config.xdg.configHome}/btop" = {
-				source = ./config/btop;
-				recursive = true;
-			};
-		};
+    file = {
+      "${config.xdg.configHome}/micro/colorschemes" = {
+        source = ./config/micro/colorschemes;
+        recursive = true;
+      };
 
-		pointerCursor = {
-			package = pkgs.rose-pine-cursor;
-			name = "BreezeX-RosePineDawn-Linux";
-			size = 24;
-	
-			gtk.enable = true;
-			x11.enable = true;
-		};
-	};
-
-	dconf.settings = {
-		"org/virt-manager/virt-manager/connections" = {
-			autoconnect = ["qemu:///system"];
-			uris = ["qemu:///system"];
-		};
+      "${config.xdg.configHome}/btop" = {
+        source = ./config/btop;
+        recursive = true;
+      };
     };
 
-	fonts.fontconfig = {
-		enable = true;
-		defaultFonts = {
-			serif = [ "NotoSerif Nerd Font" ];
-			sansSerif = [ "FiraMono Nerd Font Propo" ];
-			monospace = [ "FiraCode Nerd Font Propo" ];
-		};
-	};
+    pointerCursor = {
+      package = pkgs.rose-pine-cursor;
+      name = "BreezeX-RosePineDawn-Linux";
+      size = 24;
 
-	
-	gtk = {
-		enable = true;
-		theme = {
-			package = pkgs.flat-remix-gtk;
-			name = "Flat-Remix-GTK-Blue-Dark";
-		};
-		
-		iconTheme = {
-			name = "Papirus-Dark";
-			package = pkgs.catppuccin-papirus-folders.override {
-				flavor = "mocha";
-				accent = "blue";
-			};
-		};
-	};
+      gtk.enable = true;
+      x11.enable = true;
+    };
+  };
 
-	qt = {
-		enable = true;
-		style = {
-			name = "kvantum";
-		};
-	};
+  dconf.settings = {
+    "org/virt-manager/virt-manager/connections" = {
+      autoconnect = [ "qemu:///system" ];
+      uris = [ "qemu:///system" ];
+    };
+  };
 
-	programs = {
-		ags = {
-			enable = true;
-			configDir = ./config/ags;
-			extraPackages = with pkgs; [
-				gtksourceview
-				webkitgtk
-				accountsservice
-			];
-		};
-
-		direnv = {
-			enable = true;
-			nix-direnv.enable = true;
-
-			config = {
-
-			};
-		};
-
-		kitty = {
-			enable = true;
-
-			settings = {
-				shell_integration = true;
-			};
-
-			extraConfig = import ./config/kittyExtra.nix;
-		};
-		
-		eza = {
-			enable = true;
-			enableFishIntegration = true;
-			package = pkgs.eza;
-			git = true;
-			icons = true;
-		};
-
-		librewolf = {
-			enable = false;
-		};
-
-		firefox = {
-			enable = true;
-			policies = {
-				PasswordManagerEnabled = false;
-				PopupBlocking = true;
-				OfferToSaveLogins = false;
-				HardwareAcceleration = true;
-
-				ExtensionUpdate = true;
-				ExtensionSettings = let
-					extensionUrl = x: "https://addons.mozilla.org/firefox/downloads/latest/${x}/latest.xpi";
-				in {
-					"uBlock0@raymondhill.net" = {
-						installation_mode = "force_installed";
-						install_url = extensionUrl "ublock-origin";
-					};
-
-					"addon@darkreader.org" = {
-						installation_mode = "force_installed";
-						install_url = extensionUrl "darkreader";
-					};
-
-					"78272b6fa58f4a1abaac99321d503a20@proton.me" = {
-						installation_mode = "force_installed";
-						install_url = extensionUrl "proton-pass";
-					};
-
-					"FirefoxColor@mozilla.com" = {
-						installation_mode = "force_installed";
-						install_url = extensionUrl "firefox-color";
-					};
-				};
-			};
-		};
-
-		micro = {
-			enable = true;
-			settings = {
-				autosu = true;
-				mkparents = true;
-				colorscheme = "rose-pine";
-			};
-		};
-
-		wofi = {
-			enable = true;
-			package = pkgs.wofi;
-			settings = {
-				prompt = "Search";
-				matching = "contains";
-				insensitive = true;
-			};
-		};
-
-		fish = {
-			enable = true;
-			shellAliases = {
-				l = "ls -alh --color=auto";
-				ll = "ls -l --color=auto";
-				ls = "ls --color=auto";
-				rebuild = "~/.nixos/rebuild.sh";
-				upgrade = "~/.nixos/upgrade.sh";
-				test-rebuild = "~/.nixos/test.sh";
-			};
-
-			functions = {
-				rebuild = "/home/ayes/.nixos/rebuild.sh $argv";
-				test-rebuild = "/home/ayes/.nixos/test.sh $argv";
-				upgrade = "/home/ayes/.nixos/upgrade.sh $argv";
-
-				fish_prompt = ''
-					#Save the return status of the previous command
-					set -l last_pipestatus $pipestatus
-					set -lx __fish_last_status $status # Export for __fish_print_pipestatus.
-
-					if functions -q fish_is_root_user; and fish_is_root_user
-						printf '%s@%s %s%s%s# ' $USER (prompt_hostname) (set -q fish_color_cwd_root
-																		 and set_color $fish_color_cwd_root
-																		 or set_color $fish_color_cwd) \
-							(prompt_pwd) (set_color normal)
-					else
-						set -l status_color (set_color $fish_color_status)
-						set -l statusb_color (set_color --bold $fish_color_status)
-						set -l pipestatus_string (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
-
-						printf '[%s] %s%s@%s %s%s %s%s%s \n> ' (date "+%H:%M:%S") (set_color brblue) \
-							$USER (prompt_hostname) (set_color $fish_color_cwd) $PWD $pipestatus_string \
-							(set_color normal)
-					end
-				'';
-			};
-		};
-
-		bash = {
-			enable = true;
-			initExtra = ''
-				if [[
-					$(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" &&
-					-z ''${BASH_EXECUTION_STRING}
-				]] then
-					shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
-					exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
-				fi
-			'';
-		};
-
-		git = {
-			enable = true;
-			userName = "Ayes";
-			userEmail = "github.com.faculty419@passmail.net";
-			lfs.enable = true;
-			diff-so-fancy.enable = true;
-
-			extraConfig = {
-				user = {
-					name = "Ayes";
-					email = "github.com.faculty419@passmail.net";
-				};
-
-				credential = {
-					credentialStore = "secretservice";
-				};
-			};
-		};
-
-		hyprlock = {
-			enable = true;
-			settings = {
-				general = {
-					disable_loading_bar = false;
-					grace = 0;
-					hide_cursor = false;
-					no_fade_in = false;
-				};
-
-				background = [
-					{
-						color = "rgba(25, 20, 20, 1.0)";
-
-						blur_passes = 3;
-						blur_size = 12;
-						noise = 0.0117;
-					}
-				];
-
-				input-field = [
-					{
-						size = "300, 50";
-						position = "0, -80";
-						monitor = "";
-						dots_center = true;
-						fade_on_empty = false;
-						font_color = "rgb(202, 211, 245)";
-						inner_color = "rgb(91, 96, 120)";
-						outer_color = "rgb(24, 25, 38)";
-						outline_thickness = 3;
-						placeholder_text = ''<span foreground="##cad3f5">Password...</span>'';
-						shadow_passes = 2;
-					}
-				];
-			};
-
-		};
-	};
-
-	services = {
-		hyprpaper = {
-			enable = true;
-			settings = {
-				ipc = "on";
-				splash = false;
-
-				preload = [
-					"/home/ayes/.nixos/config/hypr/wallpapers/dark_love_wallpaper.jpg"
-					"/home/ayes/.nixos/config/hypr/wallpapers/dark_anime_wallpaper.jpg"
-					"/home/ayes/.nixos/config/hypr/wallpapers/black-rock-shooter-43606-1920x1080.jpg"
-				];
-
-				wallpaper = [
-					"DP-1, /home/ayes/.nixos/config/hypr/wallpapers/dark_love_wallpaper.jpg"
-					"DP-2, /home/ayes/.nixos/config/hypr/wallpapers/dark_anime_wallpaper.jpg"
-					"HDMI-A-1, /home/ayes/.nixos/config/hypr/wallpapers/black-rock-shooter-43606-1920x1080.jpg"
-				];
-			};
-		};
+  fonts.fontconfig = {
+    enable = true;
+    defaultFonts = {
+      serif = [ "NotoSerif Nerd Font" ];
+      sansSerif = [ "FiraMono Nerd Font Propo" ];
+      monospace = [ "FiraCode Nerd Font Propo" ];
+    };
+  };
 
 
+  gtk = {
+    enable = true;
+    theme = {
+      package = pkgs.flat-remix-gtk;
+      name = "Flat-Remix-GTK-Blue-Dark";
+    };
 
-		hypridle = {
-			enable = true;
-			settings = {
-				general = {
+    iconTheme = {
+      name = "Papirus-Dark";
+      package = pkgs.catppuccin-papirus-folders.override {
+        flavor = "mocha";
+        accent = "blue";
+      };
+    };
+  };
 
-				};
+  qt = {
+    enable = true;
+    style = {
+      name = "kvantum";
+    };
+  };
 
-				listener = [
-					{
-                        timeout = 300;
-                        on-timeout = "hyprlock";
-                    }
-				];
-			};
-		};
+  programs = {
+    ags = {
+      enable = true;
+      configDir = ./config/ags;
+      extraPackages = with pkgs; [
+        gtksourceview
+        webkitgtk
+        accountsservice
+      ];
+    };
 
-		arrpc.enable = true;
-	};
+    direnv = {
+      enable = true;
+      nix-direnv.enable = true;
 
-	wayland = {
-		windowManager.hyprland = {
-			enable = true;
-			package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+      config = { };
+    };
 
-			systemd = {
-				enable = true;
-				enableXdgAutostart = true;
-			};
+    kitty = {
+      enable = true;
 
-			xwayland = {
-				enable = true;
-			};
-		};
-	};
+      settings = {
+        shell_integration = true;
+      };
 
-	xdg.mimeApps = {
-		enable = true;
-		defaultApplications = {
-			"inode/directory" = ["pcmanfm.desktop"];
-			"application/pdf" = ["firefox.desktop"];
+      extraConfig = import ./config/kittyExtra.nix;
+    };
 
-			## Archives ##
-			"application/vnd.rar" = ["xarchiver.desktop"];
-			"application/zip" = ["xarchiver.desktop"];
-			"application/x-7z-compressed" = ["xarchiver.desktop"];
-			"application/gzip" = ["xarchiver.desktop"];
-			"application/x-tar" = ["xarchiver.desktop"];
+    eza = {
+      enable = true;
+      enableFishIntegration = true;
+      package = pkgs.eza;
+      git = true;
+      icons = true;
+    };
 
-			## Videos ##
-			"application/mp4" = ["haruna.desktop"];
+    librewolf = {
+      enable = false;
+    };
 
-			## Music ##
-			"application/mp3" = ["haruna.desktop"];
-			"application/wav" = ["haruna.desktop"];
-		};
-	};
+    firefox = {
+      enable = true;
+      policies = {
+        PasswordManagerEnabled = false;
+        PopupBlocking = true;
+        OfferToSaveLogins = false;
+        HardwareAcceleration = true;
+
+        ExtensionUpdate = true;
+        ExtensionSettings =
+          let
+            extensionUrl = x: "https://addons.mozilla.org/firefox/downloads/latest/${x}/latest.xpi";
+          in
+          {
+            "uBlock0@raymondhill.net" = {
+              installation_mode = "force_installed";
+              install_url = extensionUrl "ublock-origin";
+            };
+
+            "addon@darkreader.org" = {
+              installation_mode = "force_installed";
+              install_url = extensionUrl "darkreader";
+            };
+
+            "78272b6fa58f4a1abaac99321d503a20@proton.me" = {
+              installation_mode = "force_installed";
+              install_url = extensionUrl "proton-pass";
+            };
+
+            "FirefoxColor@mozilla.com" = {
+              installation_mode = "force_installed";
+              install_url = extensionUrl "firefox-color";
+            };
+          };
+      };
+    };
+
+    micro = {
+      enable = true;
+      settings = {
+        autosu = true;
+        mkparents = true;
+        colorscheme = "rose-pine";
+      };
+    };
+
+    wofi = {
+      enable = true;
+      package = pkgs.wofi;
+      settings = {
+        prompt = "Search";
+        matching = "contains";
+        insensitive = true;
+      };
+    };
+
+    fish = {
+      enable = true;
+      shellAliases = {
+        l = "ls -alh --color=auto";
+        ll = "ls -l --color=auto";
+        ls = "ls --color=auto";
+        rebuild = "~/.nixos/rebuild.sh";
+        upgrade = "~/.nixos/upgrade.sh";
+        test-rebuild = "~/.nixos/test.sh";
+      };
+
+      functions = {
+        rebuild = "/home/ayes/.nixos/rebuild.sh $argv";
+        test-rebuild = "/home/ayes/.nixos/test.sh $argv";
+        upgrade = "/home/ayes/.nixos/upgrade.sh $argv";
+
+        fish_prompt = ''
+          					#Save the return status of the previous command
+          					set -l last_pipestatus $pipestatus
+          					set -lx __fish_last_status $status # Export for __fish_print_pipestatus.
+
+          					if functions -q fish_is_root_user; and fish_is_root_user
+          						printf '%s@%s %s%s%s# ' $USER (prompt_hostname) (set -q fish_color_cwd_root
+          																		 and set_color $fish_color_cwd_root
+          																		 or set_color $fish_color_cwd) \
+          							(prompt_pwd) (set_color normal)
+          					else
+          						set -l status_color (set_color $fish_color_status)
+          						set -l statusb_color (set_color --bold $fish_color_status)
+          						set -l pipestatus_string (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
+
+          						printf '[%s] %s%s@%s %s%s %s%s%s \n> ' (date "+%H:%M:%S") (set_color brblue) \
+          							$USER (prompt_hostname) (set_color $fish_color_cwd) $PWD $pipestatus_string \
+          							(set_color normal)
+          					end
+          				'';
+      };
+    };
+
+    bash = {
+      enable = true;
+      initExtra = ''
+        				if [[
+        					$(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" &&
+        					-z ''${BASH_EXECUTION_STRING}
+        				]] then
+        					shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+        					exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
+        				fi
+        			'';
+    };
+
+    git = {
+      enable = true;
+      userName = "Ayes";
+      userEmail = "github.com.faculty419@passmail.net";
+      lfs.enable = true;
+      diff-so-fancy.enable = true;
+
+      extraConfig = {
+        user = {
+          name = "Ayes";
+          email = "github.com.faculty419@passmail.net";
+        };
+
+        credential = {
+          credentialStore = "secretservice";
+        };
+      };
+    };
+
+    hyprlock = {
+      enable = true;
+      settings = {
+        general = {
+          disable_loading_bar = false;
+          grace = 0;
+          hide_cursor = false;
+          no_fade_in = false;
+        };
+
+        background = [
+          {
+            color = "rgba(25, 20, 20, 1.0)";
+
+            blur_passes = 3;
+            blur_size = 12;
+            noise = 0.0117;
+          }
+        ];
+
+        input-field = [
+          {
+            size = "300, 50";
+            position = "0, -80";
+            monitor = "";
+            dots_center = true;
+            fade_on_empty = false;
+            font_color = "rgb(202, 211, 245)";
+            inner_color = "rgb(91, 96, 120)";
+            outer_color = "rgb(24, 25, 38)";
+            outline_thickness = 3;
+            placeholder_text = ''<span foreground="##cad3f5">Password...</span>'';
+            shadow_passes = 2;
+          }
+        ];
+      };
+
+    };
+  };
+
+  services = {
+    hyprpaper = {
+      enable = true;
+      settings = {
+        ipc = "on";
+        splash = false;
+
+        preload = [
+          "/home/ayes/.nixos/config/hypr/wallpapers/dark_love_wallpaper.jpg"
+          "/home/ayes/.nixos/config/hypr/wallpapers/dark_anime_wallpaper.jpg"
+          "/home/ayes/.nixos/config/hypr/wallpapers/black-rock-shooter-43606-1920x1080.jpg"
+        ];
+
+        wallpaper = [
+          "DP-1, /home/ayes/.nixos/config/hypr/wallpapers/dark_love_wallpaper.jpg"
+          "DP-2, /home/ayes/.nixos/config/hypr/wallpapers/dark_anime_wallpaper.jpg"
+          "HDMI-A-1, /home/ayes/.nixos/config/hypr/wallpapers/black-rock-shooter-43606-1920x1080.jpg"
+        ];
+      };
+    };
+
+
+
+    hypridle = {
+      enable = true;
+      settings = {
+        general = { };
+
+        listener = [
+          {
+            timeout = 300;
+            on-timeout = "hyprlock";
+          }
+        ];
+      };
+    };
+
+    arrpc.enable = true;
+  };
+
+  wayland = {
+    windowManager.hyprland = {
+      enable = true;
+      package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+
+      systemd = {
+        enable = true;
+        enableXdgAutostart = true;
+      };
+
+      xwayland = {
+        enable = true;
+      };
+    };
+  };
+
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "inode/directory" = [ "pcmanfm.desktop" ];
+      "application/pdf" = [ "firefox.desktop" ];
+
+      ## Archives ##
+      "application/vnd.rar" = [ "xarchiver.desktop" ];
+      "application/zip" = [ "xarchiver.desktop" ];
+      "application/x-7z-compressed" = [ "xarchiver.desktop" ];
+      "application/gzip" = [ "xarchiver.desktop" ];
+      "application/x-tar" = [ "xarchiver.desktop" ];
+
+      ## Videos ##
+      "application/mp4" = [ "haruna.desktop" ];
+
+      ## Music ##
+      "application/mp3" = [ "haruna.desktop" ];
+      "application/wav" = [ "haruna.desktop" ];
+    };
+  };
 }
