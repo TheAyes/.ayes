a597fc39056e07a9eb03d0c6270b08219d489bc3
ayes <github.com.faculty419@passmail.net>
2024-10-31 20:37:27 +0100

    305 current  2024-10-31 20:37:19  24.11.20241029.807e915  6.11.5-zen1                          *

--- commands.sh
+++ commands.sh
@@ -10,7 +10,7 @@ popd () {
 
 export -f pushd popd
 export nixosConfigPath="$HOME/.nixos"
-export update="nix flake update $nixosConfigPath"
+export update="nix flake update --flake $nixosConfigPath"
 export rebuild="nixos-rebuild -v --use-remote-sudo --flake $nixosConfigPath"
 
 export return=popd
