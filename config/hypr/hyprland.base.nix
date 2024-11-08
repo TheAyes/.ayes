5f50c8eb5b0b95a95c5635b8af8506b465b95656
ayes <github.com.faculty419@passmail.net>
2024-11-09 00:23:59 +0100

    315 current  2024-11-09 00:23:58  24.11.20241105.4aa3656  6.11.5-zen1                          *

--- config/hypr/hyprland.base.nix
+++ config/hypr/hyprland.base.nix
@@ -100,10 +100,11 @@
         passes = 3;
       };
 
-      drop_shadow = "yes";
-      shadow_range = 4;
-      shadow_render_power = 3;
-      #"col.shadow" = "rgba(1a1a1aee)";
+      shadow = {
+        enabled = true;
+        range = 4;
+        render_power = 3;
+      };
     };
 
     animations = {
