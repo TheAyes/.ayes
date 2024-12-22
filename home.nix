c71dd628374f277f8b0cfeb86e62465867aa37c3
ayes <github.com.faculty419@passmail.net>
2024-12-22 23:50:24 +0100

    349 current  2024-12-22 23:50:21  25.05.20241217.d3c42f1  6.12.2-zen1                          *

--- home.nix
+++ home.nix
@@ -178,9 +178,7 @@
     kitty = {
       enable = true;
 
-      settings = {
-        shell_integration = true;
-      };
+      shellIntegration.enableFishIntegration = true;
 
       extraConfig = import ./config/kittyExtra.nix;
     };
@@ -188,7 +186,7 @@
     eza = {
       enable = true;
       enableFishIntegration = true;
-      package = pkgs.eza;
+      #package = pkgs.eza;
       git = true;
       icons = "auto";
     };
@@ -272,31 +270,8 @@
         test-rebuild = "/home/ayes/.nixos/test.sh $argv";
         upgrade = "/home/ayes/.nixos/upgrade.sh $argv";
 
-        fish_prompt = ''
-          					#Save the return status of the previous command
-          					set -l last_pipestatus $pipestatus
-          					set -lx __fish_last_status $status # Export for __fish_print_pipestatus.
-
-          					if functions -q fish_is_root_user; and fish_is_root_user
-          						printf '%s@%s %s%s%s# ' $USER (prompt_hostname) (set -q fish_color_cwd_root
-          																		 and set_color $fish_color_cwd_root
-          																		 or set_color $fish_color_cwd) \
-          							(prompt_pwd) (set_color normal)
-          					else
-          						set -l status_color (set_color $fish_color_status)
-          						set -l statusb_color (set_color --bold $fish_color_status)
-          						set -l pipestatus_string (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
-
-          						printf '[%s] %s%s@%s %s%s %s%s%s \n> ' (date "+%H:%M:%S") (set_color brblue) \
-          							$USER (prompt_hostname) (set_color $fish_color_cwd) $PWD $pipestatus_string \
-          							(set_color normal)
-          					end
-          				'';
+        #fish_prompt = '' '';
       };
-
-      shellInitLast = ''
-        	clear
-      '';
     };
 
     bash = {
