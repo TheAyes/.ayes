df2115abc324b084f2d7acda07e24729d5635f33
J.Schildt <J.Schildt@bosfood.de>
2025-01-23 17:25:08 +0100

    Add WSL support and new NixOS configuration for 'leda'
    Introduced a WSL-specific NixOS configuration for the new 'leda' host, including system packages, user settings, and Fish shell customization. Updated flake inputs to include nixos-wsl and adjusted flake.lock accordingly. Removed the base configuration.nix and refined structure to support multi-host setups.

--- flake.nix
+++ flake.nix
@@ -1,6 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
   };
 
   outputs = {
@@ -11,14 +12,28 @@
     inherit (self) outputs;
   in {
     nixosConfigurations = {
+
       io = nixpkgs.lib.nixosSystem {
         specialArgs = {inherit inputs outputs;};
         modules = [
-          #./modules/user-manager
+          ./modules/user-manager
 
           ./hosts/io
+        ];
+      };
+
+      leda = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = {inherit inputs outputs;};
+        modules = [
+          inputs.nixos-wsl.nixosModules.default
+
+          #./modules/user-manager
+
+          ./hosts/leda
         ];
       };
     };
   };
 }
+
