03fa6c9a8ea6b89e9ea63dfafeaed344231538d6
ayes <github.com.faculty419@passmail.net>
2025-01-27 21:50:02 +0100

    361 current  2025-01-27 21:49:59  25.05.20250114.eb62e6a  6.12.2-zen1                          *

--- configuration.nix
+++ configuration.nix
@@ -68,6 +68,18 @@
       "video=HDMI-A-1:1920x1080@60"
     ];
 
+    kernelPatches = [
+      {
+        name = "amdgpu-ignore-ctx-privileges";
+        patch = pkgs.fetchpatch {
+          name = "cap_sys_nice_begone.patch";
+          url = "https://github.com/Frogging-Family/community-patches/raw/master/linux61-tkg/cap_sys_nice_begone.mypatch";
+          hash = "sha256-Y3a0+x2xvHsfLax/uwycdJf3xLxvVfkfDVqjkxNaYEo=";
+        };
+      }
+    ];
+
+
     plymouth = {
       enable = true;
       #theme = lib.mkForce "catppuccin-mocha";
