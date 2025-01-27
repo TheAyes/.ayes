d7055ccb0e2cbbc0c5542ea5e52042d2ed1eaf36
ayes <github.com.faculty419@passmail.net>
2025-01-27 22:01:43 +0100

    362 current  2025-01-27 22:01:41  25.05.20250114.eb62e6a  6.12.2-zen1                          *

--- configuration.nix
+++ configuration.nix
@@ -109,14 +109,14 @@
     };
 
     user.services = {
-      polkit-kde-authentication-agent-1 = {
+      hyprpolkitagent = {
         enable = true;
         wantedBy = [ "graphical-session.target" ];
         wants = [ "graphical-session.target" ];
         after = [ "graphical-session.target" ];
         serviceConfig = {
           Type = "simple";
-          ExecStart = "${pkgs.kdePackages.polkit-kde-agent-1}/libexec/polkit-kde-authentication-agent-1";
+          ExecStart = "${pkgs.hyprpolkitagent}/libexec/hyprpolkitagent";
           Restart = "on-failure";
           RestartSec = 1;
           TimeoutStopSec = 10;
@@ -446,6 +446,7 @@
       enable = true;
       remotePlay.openFirewall = true;
       dedicatedServer.openFirewall = true;
+      extest.enable = true;
 
       gamescopeSession.enable = true;
     };
