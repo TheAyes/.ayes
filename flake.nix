537db01ef7758b8ca7a79f30baae52524d50c3df
ayes <github.com.faculty419@passmail.net>
2025-02-10 23:27:34 +0100

    392 current  2025-02-10 23:27:30  25.05.20250210.b4f5d2b  6.13.1-zen1                           *

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Nixos config flake";
 
   inputs = {
-    nixpkgs_unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs_unstable.url = "github:FraGag/nixpkgs/lldb";
     nixpkgs_stable.url = "github:nixos/nixpkgs/nixos-24.11";
     #nixpkgs_workaround.url = "github:mschwaig/nixpkgs/comically-bad-rocm-workaround";
 
