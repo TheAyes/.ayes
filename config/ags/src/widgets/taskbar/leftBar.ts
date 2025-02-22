560dd90167c77db81dfa3848257c27184d7c9ce9
Ayes <68541335+TheAyes@users.noreply.github.com>
2025-02-22 19:45:31 +0000

    Remove unused configuration and widget files
    Deleted obsolete styles, TypeScript, and XML configuration files for AGS setup and widgets. These files were no longer in use and their removal reduces clutter, improving maintainability.

--- config/ags/src/widgets/taskbar/leftBar.ts
+++
@@ -1,9 +0,0 @@
-import { Monitor } from "../../../types";
-import { makePlayerWidget } from "../musicPlayer";
-
-export const makeLeftBar = (monitor: Monitor) =>
-	Widget.Box({
-		hpack: "start",
-		spacing: 8,
-		children: [makePlayerWidget()]
-	});
--- environments/development.nix
+++
@@ -1,9 +0,0 @@
-pkgs: pkgs.mkShell {
-	
-	packages = with pkgs; [
-		nodePackages_latest.nodejs
-		nodePackages_latest.pnpm
-		bun
-		typescript
-	];
-}
