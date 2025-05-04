5322f3cf47ed933384a66ce58d1845e022a7d544
Ayes <github.com.faculty419@passmail.net>
2025-05-04 11:18:18 +0200

    Update filesystem configurations and modules
    Added new filesystem mappings and adjusted boot kernel modules to include 'usb_storage'. Updated desktop manager packages by excluding redundant KDE components, introduced new program configurations, and enabled additional audio features in PipeWire. Updated `flake.lock` to track the latest revisions of dependencies.

--- presets/nixos/desktopManagers/kde.nix
+++ presets/nixos/desktopManagers/kde.nix
@@ -3,11 +3,20 @@
     xserver.enable = true;
     displayManager.sddm.enable = true;
     desktopManager.plasma6.enable = true;
+
   };
 
   environment.systemPackages = with pkgs.kdePackages; [
     kolourpaint
     kcalc
+
+    pkgs.unrar
+  ];
+
+  environment.plasma6.excludePackages = with pkgs.kdePackages; [
+    plasma-browser-integration
+    konsole
+    oxygen
   ];
 
   xdg.icons.fallbackCursorThemes = [ "breeze_cursors" ];
