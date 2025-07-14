231d3ccff6afa57f5c454151cdfa4ebe1e1c77c2
j.schildt <j.schildt@bosfood.de>
2025-07-14 15:26:36 +0000

    [41] Version 25.11.20250708.9807714 - Unknown
    Built: 2025-07-14 12:29:25

--- hosts/leda/configuration.nix
+++ hosts/leda/configuration.nix
@@ -31,10 +31,12 @@
     git = {
       enable = true;
       package = pkgs.gitFull;
-      config.credential = {
+      /*
+        config.credential = {
         helper = "${pkgs.git-credential-manager}/bin/git-credential-manager";
         credentialStore = "secretservice";
       };
+      */
     };
   };
 
@@ -48,7 +50,7 @@
     shellAliases = {
       "rebuild-switch" = "nixos-rebuild switch --use-remote-sudo -v --flake /mnt/c/Projekte/.ayes/";
       "rebuild-test" = "nixos-rebuild switch --use-remote-sudo -v --flake /mnt/c/Projekte/.ayes/";
-      "upgrade" = "command=(cd /mnt/c/Projekte/.ayes && /mnt/c/Projekte/.ayes/scripts/upgrade.sh) $command";
+      "upgrade" = "pushd /mnt/c/Projekte/.ayes && /mnt/c/Projekte/.ayes/scripts/upgrade.sh && popd";
       "bc14-push" = "git push apps/bc14 \"$(git subtree split --prefix=apps/bc14 main --rejoin):refs/heads/justin\" && git push";
       "bc14-pull-master" = "git subtree pull --prefix apps/bc14 apps/bc14 refs/heads/master";
       "bc14-object-split" = "pushd /mnt/c/Projekte/bosmono/ && bc14-pull-master && pushd /mnt/c/Projekte/bosmono/apps/bc14/BC/ && powershell.exe \"C:\\Projekte\\bosmono\\packages\\cal-ps-tools\\ObjectSplit.ps1\" obj.txt && popd && popd";
--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -1,7 +1,8 @@
 #! /run/current-system/sw/bin/bash
 DIR=$(git rev-parse --show-toplevel)
+MESSAGE=$("$DIR"/scripts/commit_message.sh)
 
 nixos-rebuild switch --sudo --flake "$DIR"
 
 git add .
-git commit -m "$("$DIR"/scripts/commit_message.sh)"
+git commit -m "$MESSAGE"
