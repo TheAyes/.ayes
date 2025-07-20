e1e5db45ed9aa816b0174014b14c841d4921055c
Ayes <github.com.faculty419@passmail.net>
2025-07-20 14:51:43 +0200

    [io gen.73] Version 25.11.20250706.1fd8bad - 6.15.2-zen1
    Built: 2025-07-19 19:58:48

--- scripts/commit_message.sh
+++ scripts/commit_message.sh
@@ -1,2 +1,2 @@
 #!/run/current-system/sw/bin/bash
-nixos-rebuild list-generations --json | jq -r '.[0] | "[\(.generation)] Version \(.nixosVersion) - \(.kernelVersion)\n\nBuilt: \(.date)"'
+nixos-rebuild list-generations --json | jq -r '.[0] | "['"$(hostname)"' gen.\(.generation)] Version \(.nixosVersion) - \(.kernelVersion)\n\nBuilt: \(.date)"'
