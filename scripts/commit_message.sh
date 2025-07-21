d9ab5ba9f0e54437eb0accff4fc40f081a6371e5
j.schildt <j.schildt@bosfood.de>
2025-07-21 12:08:40 +0000

    [leda gen.69] Version 25.11.20250706.1fd8bad - 6.6.87.2-microsoft-standard-WSL2
    Built: 2025-07-21 09:40:09

--- scripts/commit_message.sh
+++ scripts/commit_message.sh
@@ -1,2 +1,4 @@
 #!/run/current-system/sw/bin/bash
-nixos-rebuild list-generations --json | jq -r '.[0] | "['"$(hostname)"' gen.\(.generation)] Version \(.nixosVersion) - \(.kernelVersion)\n\nBuilt: \(.date)"'
+nixos-rebuild list-generations --json |
+ jq -r '.[0] | "['"$(hostname)"' gen.\(.generation)] Version \(.nixosVersion) - \(if .kernelVersion == "Unknown" then "'"$(uname -r)"'" else .kernelVersion end)\n\nBuilt: \(.date)"'
+
