f3fa18670ed87a933071ac4d2709ad2f2a0b0be1
j.schildt <j.schildt@bosfood.de>
2025-07-22 08:08:25 +0000

    [leda gen.73] Version 25.11.20250719.c87b95e - 6.6.87.2-microsoft-standard-WSL2
    Built: 2025-07-22 07:47:00

--- hosts/leda/configuration.nix
+++ hosts/leda/configuration.nix
@@ -60,6 +60,27 @@
   stylix = {
     enable = true;
     base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+    fonts = {
+      serif = {
+        package = pkgs.caladea;
+        name = "Caladea-Regular";
+      };
+
+      sansSerif = {
+        package = pkgs.encode-sans;
+        name = "EncodeSans-Regular";
+      };
+
+      monospace = {
+        package = pkgs.cascadia-code;
+        name = "CascadiaCodeNF";
+      };
+
+      emoji = {
+        package = pkgs.serenityos-emoji-font;
+        name = "SerenityOS Emoji";
+      };
+    };
   };
 
   environment = {
