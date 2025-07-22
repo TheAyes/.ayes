19139fdf51179acc7627195c4222b4c669f94171
j.schildt <j.schildt@bosfood.de>
2025-07-22 07:47:09 +0000

    [leda gen.72] Version 25.11.20250719.c87b95e - 6.6.87.2-microsoft-standard-WSL2
    Built: 2025-07-21 15:32:37

--- common/users/ayes/programs/btop.nix
+++ common/users/ayes/programs/btop.nix
@@ -3,18 +3,16 @@
     enable = true;
     package = pkgs.btop-rocm;
 
-    /*
-      settings = {
-      color_theme = "catppuccin-mocha";
+    settings = {
+      #color_theme = "catppuccin-mocha";
       theme_background = false;
       proc_gradient = false;
       update_ms = 500;
     };
-    */
   };
 
   /*
-    xdg.configFile.btop-catppuccin-theme = {
+  xdg.configFile.btop-catppuccin-theme = {
     enable = true;
     source = ../../../../assets/themes/btop/catppuccin-mocha.theme;
     target = "./btop/themes/catppuccin-mocha.theme";
