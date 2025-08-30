1fe95d4d03238dc3449b88a9f43bd8a4c7295a26
Ayes <github.com.faculty419@passmail.net>
2025-08-30 11:57:08 +0200

    [io gen.94] Version 25.11.20250814.fbcf476 - 6.16.0-zen1
    Built: 2025-08-30 10:55:06

--- presets/nixos/networking/default.nix
+++ presets/nixos/networking/default.nix
@@ -6,7 +6,13 @@
 
     firewall = {
       enable = true; # Ensure the firewall is enabled
-      #allowedTCPPorts = [ 3000 ];
+      allowedTCPPorts = [ 25565 ];
+      allowedUDPPorts = [ ];
+      extraCommands = ''
+        iptables -A INPUT -p tcp --dport 25565 -m conntrack --ctstate NEW -m limit --limit 5/min --limit-burst 10 -j ACCEPT
+        iptables -A INPUT -p tcp --dport 25565 -m conntrack --ctstate NEW -j DROP
+      '';
+
     };
 
   };
