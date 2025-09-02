c3d650d19a70930d31577a4c3825c6f00402c385
J.Schildt <J.Schildt@bosfood.de>
2025-09-02 16:02:05 +0200

    Refine networking defaults: configure custom DNS, update firewall rules

--- presets/nixos/networking/default.nix
+++ presets/nixos/networking/default.nix
@@ -1,13 +1,16 @@
-{ hostname, ... }: {
+{hostname, ...}: {
   networking = {
     hostName = hostname;
 
     networkmanager.enable = true;
+    networkmanager.dns = "none";
+
+    nameservers = ["172.64.36.1" "172.64.36.2" "2a06:98c1:54::1e:e214"];
 
     firewall = {
       enable = true; # Ensure the firewall is enabled
-      allowedTCPPorts = [ 25565 ];
-      allowedUDPPorts = [ 24454 ];
+      allowedTCPPorts = [25565];
+      allowedUDPPorts = [24454];
       extraCommands = ''
         iptables -A INPUT -p tcp --dport 25565 -m conntrack --ctstate NEW -m limit --limit 5/min --limit-burst 10 -j ACCEPT
         iptables -A INPUT -p tcp --dport 25565 -m conntrack --ctstate NEW -j DROP
@@ -15,8 +18,6 @@
       logRefusedConnections = true;
       logRefusedPackets = true;
       logReversePathDrops = true;
-
     };
-
   };
 }
