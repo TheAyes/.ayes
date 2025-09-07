22dd3b2b9cb24282ed285e55c09ea8e2f6a20f72
Ayes <github.com.faculty419@passmail.net>
2025-09-07 23:13:58 +0200

    [io gen.115] Version 25.11.20250828.dfb2f12 - 6.16.3-zen1
    Built: 2025-09-07 22:36:03

--- hosts/io/minecraft-servers.nix
+++ hosts/io/minecraft-servers.nix
@@ -66,7 +66,7 @@
   ];
 
   networking.firewall = {
-    allowedTCPPorts = [ 25565 ];
+    allowedTCPPorts = [ 25565 8100 ];
     allowedUDPPorts = [ 24454 ];
     extraCommands = ''
       iptables -A INPUT -p tcp --dport 25565 -m conntrack --ctstate NEW -m limit --limit 5/min --limit-burst 10 -j ACCEPT
