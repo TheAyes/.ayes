d1fedbe6f259844dff344c68145fb2170911ed6c
Ayes <github.com.faculty419@passmail.net>
2025-10-18 11:39:42 +0200

    [io gen.132] Version 25.11.20251015.544961d - 6.17.1-zen1
    Built: 2025-10-18 10:01:42

--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -2,11 +2,19 @@
 DIR=$(git rev-parse --show-toplevel)
 MESSAGE=$("$DIR"/scripts/commit_message.sh)
 
-if nh os switch "$DIR" --ask --diff=auto; then
-  tide configure --auto --style=Lean --prompt_colors='True color' --show_time='24-hour format' --lean_prompt_height='Two lines' --prompt_connection=Dotted --prompt_connection_andor_frame_color=Dark --prompt_spacing=Sparse --icons='Many icons' --transient=Yes
+if [ -z "$1" ]; then
+	echo "error: missing positional argument <COMMAND>"
+	echo ""
+	echo "Usage: rebuild.sh <COMMAND> [--upgrade]"
+
+	exit 1
+fi
+
+if nh os "$1" "$DIR" --ask --diff=auto $2; then
+	tide configure --auto --style=Lean --prompt_colors='True color' --show_time='24-hour format' --lean_prompt_height='Two lines' --prompt_connection=Dotted --prompt_connection_andor_frame_color=Dark --prompt_spacing=Sparse --icons='Many icons' --transient=Yes
 
 	sudo mount --bind /etc/nixos/common/users/ayes/external/xlcore ~/.xlcore
 
-  git add .
-  git commit -m "$MESSAGE"
+	git add .
+	git commit -m "$MESSAGE"
 fi
