a581a3450173dd630703963609704c7678c85432
Ayes <github.com.faculty419@passmail.net>
2025-12-23 21:06:25 +0100

    [io gen.162] Version 26.05.20251221.a653104 - 6.18.1-zen1
    Built: 2025-12-23 20:38:30

--- presets/nixos/networking/default.nix
+++ presets/nixos/networking/default.nix
@@ -6,7 +6,11 @@
     networkmanager.enable = true;
     networkmanager.dns = "none";
 
-    #nameservers = [ "172.64.36.1" "172.64.36.2" "2a06:98c1:54::1e:e29b" ];
+    nameservers = [
+      "172.64.36.1"
+      "172.64.36.2"
+      "2a06:98c1:54::1e:e29b"
+    ];
 
     firewall = {
       enable = true; # Ensure the firewall is enabled
