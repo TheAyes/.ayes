40482a634fc02aede7af7b7edd2f9e976c90289b
Ayes <github.com.faculty419@passmail.net>
2026-01-12 02:44:24 +0100

    [io gen.177] Version 26.05.20260110.1566daa - 6.18.3-zen1
    Built: 2026-01-12 01:56:31

--- hosts/janus/configuration.nix
+++ hosts/janus/configuration.nix
@@ -7,4 +7,17 @@
   imports = [
     ../../presets/nixos/locales/german.nix
   ];
+
+  fileSystems = {
+    "/boot" = {
+      device = "/dev/disk/by-label/boot";
+      fsType = "ext4";
+    };
+
+    "/" = {
+      device = "/dev/disk/by-label/nixos";
+      fsType = "ext4";
+    };
+  };
+  swapDevices = [ { device = "/dev/disk/by-label/swap"; } ];
 }
