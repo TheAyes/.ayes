9a86edda6c4a05a4abf77b40476b7f1fe921ba6b
Ayes <github.com.faculty419@passmail.net>
2026-02-22 20:27:00 +0100

    [io gen.217] Version 26.05.20260204.00c21e4 - 6.18.7-zen1
    Built: 2026-02-22 20:15:26

--- hosts/janus/reverse-proxy.nix
+++ hosts/janus/reverse-proxy.nix
@@ -18,14 +18,17 @@
           respond /.well-known/matrix/server `{"m.server": "matrix.convene.chat:443"}`
           respond /.well-known/matrix/client `{"m.homeserver": {"base_url": "https://matrix.convene.chat"}, "org.matrix.msc4143.rtc_foci": [{"type": "livekit", "livekit_service_url": "https://convene.chat/livekit/jwt"}]}`
 
-          # LiveKit JWT service
-          reverse_proxy /livekit/jwt/* http://[::1]:${toString config.services.lk-jwt-service.port}
+          # LiveKit JWT service (strip /livekit/jwt prefix)
+          handle_path /livekit/jwt/* {
+            reverse_proxy http://[::1]:${toString config.services.lk-jwt-service.port}
+          }
 
-          # LiveKit SFU (WebSocket)
-          @livekit_sfu path /livekit/sfu/*
-          reverse_proxy @livekit_sfu http://[::1]:${toString config.services.livekit.settings.port} {
-            header_up Connection {>Connection}
-            header_up Upgrade {>Upgrade}
+          # LiveKit SFU (WebSocket, strip /livekit/sfu prefix)
+          handle_path /livekit/sfu/* {
+            reverse_proxy http://[::1]:${toString config.services.livekit.settings.port} {
+              header_up Connection {>Connection}
+              header_up Upgrade {>Upgrade}
+            }
           }
         '';
       };
