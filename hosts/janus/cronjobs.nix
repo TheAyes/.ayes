313397f02cc7ece4e47153d31aa7c2ae9255749c
Ayes <github.com.faculty419@passmail.net>
2026-05-19 00:35:19 +0200

    [io gen.245] Version 26.05.20260515.d233902 - 7.0.7-zen2
    Built: 2026-05-18 18:24:41

---
+++ hosts/janus/cronjobs.nix
@@ -0,0 +1,25 @@
+{ pkgs, ... }:
+{
+  systemd.timers."upgrade-server" = {
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      OnCalendar = "daily";
+      Persistent = true;
+    };
+  };
+
+  systemd.services."upgrade-server" = {
+    description = "Pull, upgrade, push, and reboot";
+    serviceConfig = {
+      Type = "oneshot";
+      WorkingDirectory = "/etc/nixos";
+      ExecStart = pkgs.writeShellScript "upgrade-server" ''
+        set -e
+        git pull
+        /etc/nixos/scripts/rebuild.sh boot --update
+        git push
+        systemctl reboot
+      '';
+    };
+  };
+}
