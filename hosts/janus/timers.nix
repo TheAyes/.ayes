6a1da0216f044afa3da705a95535faf9562be610
Ayes <github.com.faculty419@passmail.net>
2026-05-19 00:55:43 +0200

    [io gen.246] Version 26.05.20260515.d233902 - 7.0.7-zen2
    Built: 2026-05-19 00:34:11

--- hosts/janus/timers.nix
+++ hosts/janus/timers.nix
@@ -12,13 +12,14 @@
     description = "Pull, upgrade, push, and reboot";
     serviceConfig = {
       Type = "oneshot";
+      User = "ayes";
       WorkingDirectory = "/etc/nixos";
       ExecStart = pkgs.writeShellScript "upgrade-server" ''
         set -e
         git pull
         /etc/nixos/scripts/rebuild.sh boot --update --headless
         git push
-        systemctl reboot
+        sudo systemctl reboot
       '';
     };
   };
