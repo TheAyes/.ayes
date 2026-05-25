160e5e9ed77b749604c4a5eb80373dace95bb9ed
Ayes <github.com.faculty419@passmail.net>
2026-05-25 16:05:22 +0200

    [io gen.253] Version 26.05.20260523.2991645 - 7.0.9-zen2
    Built: 2026-05-25 16:01:26

--- profiles/nixos/programs/steam.nix
+++ profiles/nixos/programs/steam.nix
@@ -1,12 +1,21 @@
-{ lib, pkgs, ... }: {
-  programs.steam = {
-    enable = true;
+{ lib, pkgs, ... }:
+{
+  programs = {
+    steam = {
+      enable = true;
 
-    extraCompatPackages = with pkgs; [
-      proton-ge-bin
-    ];
+      extraCompatPackages = with pkgs; [
+        proton-ge-bin
+      ];
 
-    remotePlay.openFirewall = lib.mkDefault true;
-    protontricks.enable = lib.mkDefault true;
+      remotePlay.openFirewall = lib.mkDefault true;
+      protontricks.enable = lib.mkDefault true;
+      gamescopeSession.enable = true;
+    };
+
+    gamescope = {
+      enable = true;
+      #capSysNice = true;
+    };
   };
 }
