880dc818129e03f1051986f83f884afa653db31e
Ayes <github.com.faculty419@passmail.net>
2026-07-19 17:07:40 +0200

    [io gen.277] Version 26.11.20260718.61b7c44 - 7.1.3-zen2
    Built: 2026-07-19 17:05:06

--- users/ayes/hosts/io/autostart.nix
+++ users/ayes/hosts/io/autostart.nix
@@ -13,20 +13,31 @@
         requiresNetwork = true;
       };
 
-      wp-dp-1 = {
-        description = "Wallpaper for DP-1";
-        command = "/etc/profiles/per-user/ayes/bin/linux-wallpaperengine --screen-root=DP-1 2459477453";
+      wallpaper-engine = {
+        description = "Wallpapers";
+        command =
+          "/etc/profiles/per-user/ayes/bin/linux-wallpaperengine --silent "
+          + "--screen-root=DP-1 --bg 3731183557 "
+          + "--screen-root=HDMI-A-1 --bg 2473589076 "
+          + "--screen-root=DP-2 --bg 2217899039";
       };
 
-      wp-hdmi-a-1 = {
-        description = "Wallpaper for HDMI-A1";
-        command = "/etc/profiles/per-user/ayes/bin/linux-wallpaperengine --screen-root=HDMI-A-1 2594686226";
-      };
+      /*
+        wp-dp-1 = {
+          description = "Wallpaper for DP-1";
+          command = "/etc/profiles/per-user/ayes/bin/linux-wallpaperengine --screen-root=DP-1 3731183557 --silent";
+        };
 
-      wp-dp-2 = {
-        description = "Wallpaper for DP-2";
-        command = "/etc/profiles/per-user/ayes/bin/linux-wallpaperengine --screen-root=DP-2 2744538407";
-      };
+        wp-hdmi-a-1 = {
+          description = "Wallpaper for HDMI-A1";
+          command = "/etc/profiles/per-user/ayes/bin/linux-wallpaperengine --screen-root=HDMI-A-1 2473589076";
+        };
+
+        wp-dp-2 = {
+          description = "Wallpaper for DP-2";
+          command = "/etc/profiles/per-user/ayes/bin/linux-wallpaperengine --screen-root=DP-2 2217899039";
+        };
+      */
     };
   };
 }
