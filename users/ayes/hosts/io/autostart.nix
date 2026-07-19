ed276cd826e34835fe5c13b1b5e089ddbf0a7892
Ayes <github.com.faculty419@passmail.net>
2026-07-19 14:22:24 +0200

    [io gen.274] Version 26.11.20260718.61b7c44 - 7.1.3-zen2
    Built: 2026-07-19 14:22:13

--- users/ayes/hosts/io/autostart.nix
+++ users/ayes/hosts/io/autostart.nix
@@ -12,6 +12,21 @@
         command = "/etc/profiles/per-user/ayes/bin/vesktop";
         requiresNetwork = true;
       };
+
+      wp-dp-1 = {
+        description = "Wallpaper for DP-1";
+        command = "/etc/profiles/per-user/ayes/bin/linux-wallpaperengine --screen-root=DP-1 2459477453";
+      };
+
+      wp-hdmi-a-1 = {
+        description = "Wallpaper for HDMI-A1";
+        command = "/etc/profiles/per-user/ayes/bin/linux-wallpaperengine --screen-root=DP-1 2594686226";
+      };
+
+      wp-dp-2 = {
+        description = "Wallpaper for DP-2";
+        command = "/etc/profiles/per-user/ayes/bin/linux-wallpaperengine --screen-root=DP-1 2744538407";
+      };
     };
   };
 }
