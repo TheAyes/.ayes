f7dc7311a9a56fab9446716b68da9e4c61dd8b6d
j.schildt <j.schildt@bosfood.de>
2026-07-21 10:31:03 +0000

    Refactor JVM opts

--- hosts/io/minecraft-servers/prominence-2-base.nix
+++ hosts/io/minecraft-servers/prominence-2-base.nix
@@ -13,14 +13,65 @@ in
       name:
       let
         allowedRam = "16G";
+
+        jvmOpts = lib.concatStringsSep " " [
+          # Heap sizing
+          "-Xmx${allowedRam}"
+          "-Xms${allowedRam}"
+
+          # Unlock non-standard options used below
+          "-XX:+UnlockExperimentalVMOptions"
+          "-XX:+UnlockDiagnosticVMOptions"
+
+          # General runtime tuning
+          "-XX:+AlwaysActAsServerClassMachine"
+          "-XX:+AlwaysPreTouch"
+          "-XX:+DisableExplicitGC"
+          "-XX:+UseNUMA"
+          "-XX:+UseVectorCmov"
+          "-XX:+PerfDisableSharedMem"
+          "-XX:+UseFastUnorderedTimeStamps"
+          "-XX:+UseCriticalJavaThreadPriority"
+          "-XX:ThreadPriorityPolicy=1"
+          "-XX:AllocatePrefetchStyle=3"
+
+          # JIT / code cache
+          "-XX:NmethodSweepActivity=1"
+          "-XX:ReservedCodeCacheSize=400M"
+          "-XX:NonNMethodCodeHeapSize=12M"
+          "-XX:ProfiledCodeHeapSize=194M"
+          "-XX:NonProfiledCodeHeapSize=194M"
+          "-XX:-DontCompileHugeMethods"
+          "-XX:MaxNodeLimit=240000"
+          "-XX:NodeLimitFudgeFactor=8000"
+
+          # G1 garbage collector
+          "-XX:+UseG1GC"
+          "-XX:MaxGCPauseMillis=130"
+          "-XX:G1NewSizePercent=28"
+          "-XX:G1HeapRegionSize=16M"
+          "-XX:G1ReservePercent=20"
+          "-XX:G1MixedGCCountTarget=3"
+          "-XX:InitiatingHeapOccupancyPercent=10"
+          "-XX:G1MixedGCLiveThresholdPercent=90"
+          "-XX:G1RSetUpdatingPauseTimePercent=0"
+          "-XX:SurvivorRatio=32"
+          "-XX:MaxTenuringThreshold=1"
+          "-XX:G1SATBBufferEnqueueingThresholdPercent=30"
+          "-XX:G1ConcMarkStepDurationMillis=5"
+          "-XX:G1ConcRSHotCardLimit=16"
+          "-XX:G1ConcRefinementServiceIntervalMillis=150"
+          "-XX:ConcGCThreads=6"
+        ];
       in
       {
+
         "${name}" = {
 
           enable = true;
           autoStart = true;
           package = pkgs.fabricServers.fabric-1_20_1.override { jre_headless = pkgs.temurin-jre-bin; };
-          jvmOpts = "-Xmx${allowedRam} -Xms${allowedRam} -XX:+UnlockExperimentalVMOptions -XX:+UnlockDiagnosticVMOptions -XX:+AlwaysActAsServerClassMachine -XX:+AlwaysPreTouch -XX:+DisableExplicitGC -XX:+UseNUMA -XX:NmethodSweepActivity=1 -XX:ReservedCodeCacheSize=400M -XX:NonNMethodCodeHeapSize=12M -XX:ProfiledCodeHeapSize=194M -XX:NonProfiledCodeHeapSize=194M -XX:-DontCompileHugeMethods -XX:MaxNodeLimit=240000 -XX:NodeLimitFudgeFactor=8000 -XX:+UseVectorCmov -XX:+PerfDisableSharedMem -XX:+UseFastUnorderedTimeStamps -XX:+UseCriticalJavaThreadPriority -XX:ThreadPriorityPolicy=1 -XX:AllocatePrefetchStyle=3 -XX:+UseG1GC -XX:MaxGCPauseMillis=130 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=28 -XX:G1HeapRegionSize=16M -XX:G1ReservePercent=20 -XX:G1MixedGCCountTarget=3 -XX:InitiatingHeapOccupancyPercent=10 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=0 -XX:SurvivorRatio=32 -XX:MaxTenuringThreshold=1 -XX:G1SATBBufferEnqueueingThresholdPercent=30 -XX:G1ConcMarkStepDurationMillis=5 -XX:G1ConcRSHotCardLimit=16 -XX:G1ConcRefinementServiceIntervalMillis=150 -XX:ConcGCThreads=6";
+          inherit jvmOpts;
 
           serverProperties = {
             force-gamemode = true;
